91cb5f33665e4d673e6155174cc69e6bbc055ed5
Kevin Kylon <kylon99@gmail.com>
2026-04-13 21:12:29 -0700

    Fix CS0027 for this not allowed in static uses of nameof

--- AlternativePlay/UI/PlayModeSelectOption.cs
+++ AlternativePlay/UI/PlayModeSelectOption.cs
@@ -28,13 +28,13 @@ namespace AlternativePlay.UI
             this.DeleteCallback = deleteCallback;
         }
 
-        [UIValue(nameof(this.Mode))]
+        [UIValue(nameof(Mode))]
         public string Mode { get; set; }
 
-        [UIValue(nameof(this.SelectedColor))]
+        [UIValue(nameof(SelectedColor))]
         public string SelectedColor => this.configurationData.Selected == this.Index ? "#FFFFFF" : "#7F7F7F";
 
-        [UIAction(nameof(this.OnDeleteClicked))]
+        [UIAction(nameof(OnDeleteClicked))]
         public void OnDeleteClicked()
         {
             if (this.DeleteCallback != null) this.DeleteCallback(this.Index);
@@ -58,29 +58,29 @@ namespace AlternativePlay.UI
             return this.IconSummary.GameModifierIcons[index];
         }
 
-        [UIValue(nameof(this.PlayModeIcon00))]
+        [UIValue(nameof(PlayModeIcon00))]
         public string PlayModeIcon00 => this.GetPlayModeIcon(0);
-        [UIValue(nameof(this.PlayModeIcon01))]
+        [UIValue(nameof(PlayModeIcon01))]
         public string PlayModeIcon01 => this.GetPlayModeIcon(1);
-        [UIValue(nameof(this.PlayModeIcon02))]
+        [UIValue(nameof(PlayModeIcon02))]
         public string PlayModeIcon02 => this.GetPlayModeIcon(2);
-        [UIValue(nameof(this.PlayModeIcon03))]
+        [UIValue(nameof(PlayModeIcon03))]
         public string PlayModeIcon03 => this.GetPlayModeIcon(3);
 
-        [UIValue(nameof(this.TrackerIcon00))]
+        [UIValue(nameof(TrackerIcon00))]
         public string TrackerIcon00 => this.GetTrackerIcon(0);
-        [UIValue(nameof(this.TrackerIcon01))]
+        [UIValue(nameof(TrackerIcon01))]
         public string TrackerIcon01 => this.GetTrackerIcon(1);
 
-        [UIValue(nameof(this.GameModifierIcon00))]
+        [UIValue(nameof(GameModifierIcon00))]
         public string GameModifierIcon00 => this.GetGameModifierIcon(0);
-        [UIValue(nameof(this.GameModifierIcon01))]
+        [UIValue(nameof(GameModifierIcon01))]
         public string GameModifierIcon01 => this.GetGameModifierIcon(1);
-        [UIValue(nameof(this.GameModifierIcon02))]
+        [UIValue(nameof(GameModifierIcon02))]
         public string GameModifierIcon02 => this.GetGameModifierIcon(2);
-        [UIValue(nameof(this.GameModifierIcon03))]
+        [UIValue(nameof(GameModifierIcon03))]
         public string GameModifierIcon03 => this.GetGameModifierIcon(3);
-        [UIValue(nameof(this.GameModifierIcon04))]
+        [UIValue(nameof(GameModifierIcon04))]
         public string GameModifierIcon04 => this.GetGameModifierIcon(4);
 
     }
